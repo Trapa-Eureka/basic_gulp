fdf7995f41b55b97557b75c6ce63c4ae683aaef7
JINHO SON <son199@hanmail.net>
2020-06-20 16:16:41 +0900

    WebServer & Watch Files

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,9 +1,11 @@
 import gulp from "gulp";
 import gpug from "gulp-pug";
+import we from "gulp-webserver";
 // import del from "del";
 
 const routes = {
   pug: {
+    watch: "src/**/*.pug",
     src: "src/*.pug",
     dest: "build",
   },
@@ -17,6 +19,15 @@ const pug = () =>
 //const prepare = gulp.series([clean]);
 //* 주석처리 한 것은 차후 실수해서 지우고 다시 빌드할 필요가 생길때. prepare는 dev에 넣는다.
 
+const webserver = () =>
+  gulp.src("build").pipe(we({ livereload: true, open: true }));
+
+const watch = () => {
+  gulp.watch(routes.pug.watch, pug);
+};
+
 const assets = gulp.series([pug]);
 
-export const dev = gulp.series([assets]);
+const postDev = gulp.parallel([webserver, watch]);
+
+export const dev = gulp.series([assets, postDev]);
