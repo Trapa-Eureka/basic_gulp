94963b5de695dbd537056faffae3a6b8ee507ed3
JINHO SON <son199@hanmail.net>
2020-06-21 15:17:40 +0900

    image Opt Error(fixing)

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,7 +1,8 @@
 import gulp from "gulp";
 import gpug from "gulp-pug";
-import we from "gulp-webserver";
 // import del from "del";
+import ws from "gulp-webserver";
+import image from "gulp-image";
 
 const routes = {
   pug: {
@@ -9,25 +10,33 @@ const routes = {
     src: "src/*.pug",
     dest: "build",
   },
+  img: {
+    src: "src/img/*",
+    dest: "build/img",
+  },
 };
 
 const pug = () =>
   gulp.src(routes.pug.src).pipe(gpug()).pipe(gulp.dest(routes.pug.dest));
 
 //const clean = () => del(["build"]);
-
-//const prepare = gulp.series([clean]);
 //* 주석처리 한 것은 차후 실수해서 지우고 다시 빌드할 필요가 생길때. prepare는 dev에 넣는다.
 
 const webserver = () =>
-  gulp.src("build").pipe(we({ livereload: true, open: true }));
+  gulp.src("build").pipe(ws({ livereload: true, open: true }));
+
+const img = () =>
+  gulp.src(routes.img.src).pipe(image()).pipe(gulp.dest(routes.img.dest));
 
 const watch = () => {
   gulp.watch(routes.pug.watch, pug);
+  gulp.watch(routes.img.src, img);
 };
+
+const prepare = gulp.series([img]);
 
 const assets = gulp.series([pug]);
 
-const postDev = gulp.parallel([webserver, watch]);
+const live = gulp.parallel([webserver, watch]);
 
-export const dev = gulp.series([assets, postDev]);
+export const dev = gulp.series([prepare, assets, live]);
