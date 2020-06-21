127ca2fda862150c48b3611d7c6dd82e2eca9931
JINHO SON <son199@hanmail.net>
2020-06-22 01:33:01 +0900

    add footnote

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,6 +4,7 @@ import gpug from "gulp-pug";
 import ws from "gulp-webserver";
 import image from "gulp-image";
 
+// local routes for files
 const routes = {
   pug: {
     watch: "src/**/*.pug",
@@ -16,27 +17,35 @@ const routes = {
   },
 };
 
+// cleaning previous compiled results
+// 주석처리 한 것은 차후 실수해서 지우고 다시 빌드할 필요가 생길때. prepare는 dev에 넣는다.
 //const clean = () => del(["build/"]);
-//* 주석처리 한 것은 차후 실수해서 지우고 다시 빌드할 필요가 생길때. prepare는 dev에 넣는다.
 
+// compiling pug.js to html - https://www.npmjs.com/package/gulp-pug
+// compiled results at ./build
 const pug = () =>
   gulp.src(routes.pug.src).pipe(gpug()).pipe(gulp.dest(routes.pug.dest));
 
+// running webserver funct options: https://www.npmjs.com/package/gulp-webserver#options
 const webserver = () =>
   gulp.src("build").pipe(ws({ livereload: true, open: true }));
 
+// image optimization
+// compiled results are at ./build
 const img_opt = () =>
   gulp.src(routes.img.src).pipe(image()).pipe(gulp.dest(routes.img.dest));
 
+// watch is web reloader on save at .pug files
+// watch compilation of pug_to_html task: https://gulpjs.com/docs/en/api/watch
 const watch = () => {
   gulp.watch(routes.pug.watch, pug);
   gulp.watch(routes.img.src, img_opt);
 };
 
+// grouping actions: series is sequential and parellel is simultaneous
 const prepare = gulp.series([img_opt]);
-
 const assets = gulp.series([pug]);
-
 const postDev = gulp.parallel([webserver, watch]);
 
+// common and all action groups in the series
 export const dev = gulp.series([prepare, assets, postDev]);
