b1c41c35bf0e26711df7d2e1330c8f85fcb46eab
JINHO SON <son199@hanmail.net>
2020-06-22 00:30:23 +0900

    error solve - reinstall : npm install gulp-install -D

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -16,27 +16,27 @@ const routes = {
   },
 };
 
+//const clean = () => del(["build/"]);
+//* 주석처리 한 것은 차후 실수해서 지우고 다시 빌드할 필요가 생길때. prepare는 dev에 넣는다.
+
 const pug = () =>
   gulp.src(routes.pug.src).pipe(gpug()).pipe(gulp.dest(routes.pug.dest));
-
-//const clean = () => del(["build"]);
-//* 주석처리 한 것은 차후 실수해서 지우고 다시 빌드할 필요가 생길때. prepare는 dev에 넣는다.
 
 const webserver = () =>
   gulp.src("build").pipe(ws({ livereload: true, open: true }));
 
-const img = () =>
+const img_opt = () =>
   gulp.src(routes.img.src).pipe(image()).pipe(gulp.dest(routes.img.dest));
 
 const watch = () => {
   gulp.watch(routes.pug.watch, pug);
-  gulp.watch(routes.img.src, img);
+  gulp.watch(routes.img.src, img_opt);
 };
 
-const prepare = gulp.series([img]);
+const prepare = gulp.series([img_opt]);
 
 const assets = gulp.series([pug]);
 
-const live = gulp.parallel([webserver, watch]);
+const postDev = gulp.parallel([webserver, watch]);
 
-export const dev = gulp.series([prepare, assets, live]);
+export const dev = gulp.series([prepare, assets, postDev]);
